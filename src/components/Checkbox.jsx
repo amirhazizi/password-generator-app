80dddaaec20f133fa7c67868210f5283a3b315e2
amir <amirazizihosein@gmail.com>
2023-04-08 20:49:22 +0430

    setup function

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,8 +1,18 @@
 import { AiFillCheckSquare } from "react-icons/ai"
-const Checkbox = ({ label }) => {
+const Checkbox = ({ label, setCheck, template }) => {
   return (
     <label className='checkbox-container flex items-center '>
-      <input type='checkbox' />
+      <input
+        type='checkbox'
+        onChange={() => {
+          setCheck(({ value }) => {
+            if (value) {
+              return { value: !value, text: "" }
+            }
+            return { value: !value, text: template }
+          })
+        }}
+      />
       <span className='checkmark mr-4'></span>
       <p>{label}</p>
     </label>
